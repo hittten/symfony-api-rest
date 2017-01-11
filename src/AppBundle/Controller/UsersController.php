a489ef601ae073635535399331cd12294fe0b1dc
Giberto López Ambrosino <gilberto.amb@gmail.com>
2017-01-11 18:26:13 +0000

    temp

---
+++ src/AppBundle/Controller/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class UsersController extends FOSRestController
+{
+    public function getUsersAction()
+    {
+//        $data = ...; // get data, in this case list of users.
+//        $view = $this->view($data, 200)
+//            ->setTemplate("MyBundle:Users:getUsers.html.twig")
+//            ->setTemplateVar('users')
+//        ;
+//
+//        return $this->handleView($view);
+    }
+}
---
+++ src/AppBundle/Entity/AccessToken.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\OAuthServerBundle\Entity\AccessToken as BaseAccessToken;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AccessToken
+ *
+ * @ORM\Table(name="user_access_token")
+ * @ORM\Entity
+ */
+class AccessToken extends BaseAccessToken
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var Client
+     *
+     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $client;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    protected $user;
+}
